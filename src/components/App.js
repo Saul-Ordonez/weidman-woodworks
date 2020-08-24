6090bfea519550b4a3c4d327b3ea980fdd99df24
Saul-Ordonez <sauleordonez@gmail.com>
2020-08-24 15:43:28 -0700

    fix routing issue and import component files for other pages

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import NavBar from './NavBar';
 import Home from './Home';
+import About from './About';
+import Contact from './Contact';
+import Gallery from './Gallery';
 
 class App extends React.Component {
 
@@ -12,6 +15,9 @@ class App extends React.Component {
         <NavBar />
         <Switch>
           <Route path='/' exact component={Home} />
+          <Route path='/aboutpage' component={About} />
+          <Route path='/gallerypage' component={Gallery} />
+          <Route path= '/contactpage' component={Contact} />
         </Switch>
       </React.Fragment>
     );
