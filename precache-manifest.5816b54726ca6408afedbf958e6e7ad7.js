71ddebc3d77f2db562bc5b71e0826c33798a9560
Saul-Ordonez <sauleordonez@gmail.com>
2020-08-27 12:45:45 -0700

    Updates

---
+++ precache-manifest.5816b54726ca6408afedbf958e6e7ad7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a942d3314bda712d81b9d09dd0c105c",
+    "url": "/weidman-woodworks/index.html"
+  },
+  {
+    "revision": "19c2479cc436254db52f",
+    "url": "/weidman-woodworks/static/css/main.eae45001.chunk.css"
+  },
+  {
+    "revision": "c8caa8e70473635a8243",
+    "url": "/weidman-woodworks/static/js/2.0f1b1ef1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/weidman-woodworks/static/js/2.0f1b1ef1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19c2479cc436254db52f",
+    "url": "/weidman-woodworks/static/js/main.29146184.chunk.js"
+  },
+  {
+    "revision": "94b914f7e2cf6545f338",
+    "url": "/weidman-woodworks/static/js/runtime-main.6b4b48cf.js"
+  }
+]);
