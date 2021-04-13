e1aa7d31c1119e6775de6fe5675a5d54c2bc00ee
Tyler C. Yates <tyates907@gmail.com>
2021-04-13 15:14:36 -0700

    fill/style example testimonial

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import Testimonial from './Testimonial'
 
 const Home = () => {
   const titleStyle = {
     marginLeft: 20,
   }
-
   return (
     <React.Fragment>
       <h1 style={titleStyle}>Weidman Woodworks</h1>
+      <Testimonial author='John Doe' quote='Donec id lorem non nunc blandit molestie sit amet in massa. Phasellus ac ultrices mi. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Maecenas viverra.'/>
     </React.Fragment>
   );
 }
--- src/components/Testimonial.jsx
+++ src/components/Testimonial.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 
+export default function Testimonial(props) {
 
+    const testimonialStyle = {
+        color: 'black',
+        border: '1px solid black',
+        padding: '.5em',
+        margin: '.5em',
+        maxWidth: '500px',
+    }
 
-export default function Testimonial() {
   return (
-    <div>
-        
+    <div style={testimonialStyle}>
+        <p>{props.quote}</p>
+        <p>- {props.author}</p>
     </div>
   );
 }
