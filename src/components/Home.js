4fd8eda11b33019a896242cbb7ab932de693b926
Tyler C. Yates <tyates907@gmail.com>
2021-04-13 16:16:32 -0700

    remove testing of component

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,7 +8,6 @@ const Home = () => {
   return (
     <React.Fragment>
       <h1 style={titleStyle}>Weidman Woodworks</h1>
-      <Testimonial author='John Doe' quote='Donec id lorem non nunc blandit molestie sit amet in massa. Phasellus ac ultrices mi. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Maecenas viverra.'/>
     </React.Fragment>
   );
 }
