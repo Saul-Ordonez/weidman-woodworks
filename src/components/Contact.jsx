3503cec0186463506e82343570bccd112ee7b384
Saul-Ordonez <sauleordonez@gmail.com>
2021-04-19 15:44:31 -0700

    create local node server and contact form to test out email contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,15 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Contact = () => {
-  const titleStyle = {
-    marginLeft: 20,
-  }
-
+  const [status, setStatus] = useState("Submit");
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setStatus("Sending...");
+    const { name, email, message } = e.target.elements;
+    let details = {
+      name: name.value,
+      email: email.value,
+      message: message.value,
+    };
+    let response = await fetch("http://localhost:5000/contact", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json;charset=utf-8",
+      },
+      body: JSON.stringify(details),
+    });
+    setStatus("Submit");
+    let result = await response.json();
+    alert(result.status);
+  };
   return (
-    <React.Fragment>
-      <h1 style={titleStyle}>This is the Contact page</h1>
-    </React.Fragment>
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="name">Name:</label>
+        <input type="text" id="name" required />
+      </div>
+      <div>
+        <label htmlFor="email">Email:</label>
+        <input type="email" id="email" required />
+      </div>
+      <div>
+        <label htmlFor="message">Message:</label>
+        <textarea id="message" required />
+      </div>
+      <button type="submit">{status}</button>
+    </form>
   );
-}
+};
 
 export default Contact;
