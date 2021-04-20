e9ff2104c664767b6cf78a3a61d26222625ee0bd
Saul-Ordonez <sauleordonez@gmail.com>
2021-04-20 09:35:17 -0700

    add functionality to properly POST request to selected email

--- server.js
+++ server.js
@@ -24,3 +24,24 @@ contactEmail.verify((error) => {
     console.log("Ready to Send");
   }
 });
+
+router.post("/contact", (req, res) => {
+  const name = req.body.name;
+  const email = req.body.email;
+  const message = req.body.message;
+  const mail = {
+    from: name,
+    to: "sauleordonez@gmail.com",
+    subject: "Contact Form Submission",
+    html: `<p>Name: ${name}</p>
+           <p>Email: ${email}</p>
+           <p>Message: ${message}</p>`,
+  };
+  contactEmail.sendMail(mail, (error) => {
+    if (error) {
+      res.json({ status: "ERROR" });
+    } else {
+      res.json({ status: "Message Sent" });
+    }
+  });
+});
